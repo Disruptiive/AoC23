c7733f95df2ef047df2837aff1a6778a089d55f5
Antonis <atzatzidis@gmail.com>
2023-12-08 21:56:54 +0200

    added day 8

--- day8.cpp
+++ day8.cpp
@@ -27,7 +27,26 @@ class Node{
 
 template <typename T>
 Node<T>* findNode(Node<T>* root, T value){
-    return root;
+    Node<T>* ptr;
+    //cout << root->data << endl;
+    if(root != nullptr)
+    {
+        if(value == root->data)
+        {
+            return root;
+        }
+        ptr = findNode(root->left,value);
+        if (ptr != nullptr){
+            return ptr;
+        }
+        else{
+            return findNode(root->right, value);
+        }
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 
 
@@ -35,14 +54,33 @@ Node<T>* findNode(Node<T>* root, T value){
 int main(){
     vector<string> lines = fileSplitLines("day8.txt");
     string directions = lines[0];
-    stringstream ss(lines[2]);
+    stringstream init_ss(lines[2]);
     string root, l, r;
-    ss >> root;
-    ss.ignore(std::numeric_limits<std::streamsize>::max(), '(');
-    ss >> l;
-    l = l.substr(0,l.size()-1);
-    ss >> r;
-    r = r.substr(0,r.size()-1);
+    init_ss >> root;
+    Node<string>* rootNode = new Node<string>(root);
+    cout << "root: " << root <<endl;
+    for(size_t i = 2; i < lines.size();i++){
+        string node;
+        stringstream ss(lines[i]);
+        ss >> node;
+        ss.ignore(std::numeric_limits<std::streamsize>::max(), '(');
+        ss >> l;
+        l = l.substr(0,l.size()-1);
+        ss >> r;
+        r = r.substr(0,r.size()-1);
+
+        Node<string>* found_node = findNode(rootNode,node);
+        if (found_node == nullptr){
+            cout << "tf" << endl;
+        }
+        else{
+            cout << "found" <<endl;
+            found_node->left = new Node<string>(l);
+            found_node->right = new Node<string>(r);
+        }
+    }
+
+   
     
     return 0;
 }
